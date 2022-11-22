fd24aec72ab272665d4a34628c81cc7715735b43
Kallan Gerard <36556982+kallangerard@users.noreply.github.com>
2022-11-22 08:02:48 +0000

    Pending changes exported from your codespace

--- data/dev/providers.tf
+++ data/dev/providers.tf
@@ -1,9 +1,1 @@
-provider "postgresql" {
-  host            = "postgres"
-  port            = 5432
-  database        = "postgres"
-  username        = "postgres"
-  password        = "postgres"
-  sslmode         = "require"
-  connect_timeout = 15
-}
+../common/providers.tf
