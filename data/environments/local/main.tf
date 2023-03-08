ec6edb37af4da234c9286ea6c06e4b5261d7baa4
Kallan Gerard <kallan.gerard@mechanicalrock.io>
2023-03-08 20:01:33 +0800

    Add role grant

--- data/environments/local/main.tf
+++ data/environments/local/main.tf
@@ -4,3 +4,14 @@ module "stack" {
   application_role_password = "YXBwbGljYXRpb24K"
 }
 
+provider "postgresql" {
+  host            = "localhost"
+  port            = 5432
+  database        = "postgres"
+  username        = "postgres"
+  password        = "postgres"
+  sslmode         = "disable"
+  connect_timeout = 15
+}
+
+
--- data/stack/v1/postgres.tf
+++ data/stack/v1/postgres.tf
@@ -1,12 +1,20 @@
-resource "postgresql_role" "application_role" {
-  name     = "application"
+resource "postgresql_database" "todo" {
+  name              = "todo"
+  owner             = "postgres"
+  connection_limit  = -1
+  allow_connections = true
+}
+
+resource "postgresql_role" "todo_application" {
+  name     = "todo_application"
   login    = true
   password = var.application_role_password
 }
 
-resource "postgresql_database" "application_database" {
-  name              = "application"
-  owner             = "application"
-  connection_limit  = -1
-  allow_connections = true
+resource "postgresql_grant" "todo_application" {
+  role              = "todo_application"
+  database          = "todo"
+  privileges        = ["ALL"]
+  object_type       = "database"
+  with_grant_option = true
 }
